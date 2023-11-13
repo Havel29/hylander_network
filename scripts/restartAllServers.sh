88268d03912fd20f2b700ebdbad210a95759c8e3
Havel29 <boscarato.luca@gmail.com>
2023-11-13 20:01:07 +0100

    Adding script to restart all scripts

---
+++ scripts/restartAllServers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Cambiate i nomi inserendo i nomi degli screen
+servers=("Bungeecord" "Vanilla" "Test" "Test2" "Test3")
+
+
+restart_server() {
+    screen_name=$1
+    echo "Restarting $screen_name..."
+    screen -S $screen_name -X stuff "stop^M"  #Digita il comando stop all'interno del terminale del server
+    sleep 10  #Attendi la terminazione del server
+    screen -S $screen_name -X stuff "./start_script.sh^M"  #Fai ripartire il server (cambiate il nome a seconda del file shell che usate per startare il server)
+}
+
+#Ripeti per tutti i server
+for server in "${servers[@]}"; do
+    restart_server $server
+done
+
+echo "Tutti i server sono stati riavviati correttamente."
